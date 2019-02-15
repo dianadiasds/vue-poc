8ddbffc762f71a01cb5b720d377b6230ab8e5889
Diana Dias <dianadiasds@gmail.com>
2019-02-15 18:26:47 +0100

    assignment06

---
+++ assignment06/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container" id="app">
+    <Header/>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <List/>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+       <Update/>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12">
+       <Footer/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Header"
+  import List from "./components/List"
+  import Update from "./components/Update"
+  import Footer from "./components/Footer"
+
+  export default {
+
+    components: { Header, List , Update, Footer},
+  }
+</script>
+
+<style>
+
+</style>
---
+++ assignment06/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>
+    <p>All Servers are managed here</p>
+  </footer>
+</template>
+
+<script>
+
+</script>
