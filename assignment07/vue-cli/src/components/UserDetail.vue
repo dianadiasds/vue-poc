0f4a0f7a1fc9d74bcd7c8f36ca68f94dbc37201a
Diana Dias <dianadiasds@gmail.com>
2019-02-20 22:40:23 +0100

    update

---
+++ assignment07/vue-cli/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>My name: {{ switchName() }}</p>
+        <button @click="resetName">reset name</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      myName: {
+        type: String,
+        require: true
+      }
+    },
+    methods: {
+       switchName (){
+        return this.myName.split("").reverse().join("")
+      },
+      resetName () {
+         this.myName = "Diana";
+         this.$emit('nameWasReset', this.myName)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
