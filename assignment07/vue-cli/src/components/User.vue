18013c5e90fb070d617f9b0dfe168c003e9c6408
Diana Dias <dianadiasds@gmail.com>
2019-03-19 17:55:04 +0100

    update

--- assignment07/vue-cli/src/components/User.vue
+++ assignment07/vue-cli/src/components/User.vue
@@ -1,43 +1,57 @@
 <template>
-    <div class="component">
-        <h1>The User Component</h1>
-        <p>I'm an awesome User!</p>
-        <button @click="changeName">Change my name</button>
-        <p>{{ name }}</p>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
-            </div>
-        </div>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <p>Name is {{ name }}</p>
+    <p>Age is {{ age }}</p>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail
+          :myName="name"
+          @nameWasReset="name = $event"
+          :resetFn="resetName"
+          :userAge="age"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit
+          :userAge="age"
+          @ageWasEdited="age = $event"
+        ></app-user-edit>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
 
-    export default {
-        data: function () {
-          return {name: 'Diana'}
-        },
-        methods: {
-          changeName(){
-            this.name = 'Dias'
-          }
-        },
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
+  export default {
+    data: function () {
+      return {
+        name: 'Diana',
+        age: 35
+      };
+    },
+    methods: {
+      changeName() {
+        this.name = 'Anna';
+      },
+      resetName() {
+        this.name = 'Diana';
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
     }
+  }
 </script>
 
 <style scoped>
-    div {
-        background-color: lightblue;
-    }
+  div {
+    background-color: lightblue;
+  }
 </style>
--- assignment07/vue-cli/src/components/UserDetail.vue
+++ assignment07/vue-cli/src/components/UserDetail.vue
@@ -1,34 +1,44 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-        <p>My name: {{ switchName() }}</p>
-        <button @click="resetName">reset name</button>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+    <p>User Age: {{ userAge }}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetFn()">Reset Name</button>
+  </div>
 </template>
 
 <script>
+  import { eventBus } from '../main';
+
   export default {
     props: {
       myName: {
-        type: String,
-        require: true
-      }
+        type: String
+      },
+      resetFn: Function,
+      userAge: Number
     },
     methods: {
-       switchName (){
-        return this.myName.split("").reverse().join("")
+      switchName() {
+        return this.myName.split("").reverse().join("");
       },
-      resetName () {
-         this.myName = "Diana";
-         this.$emit('nameWasReset', this.myName)
+      resetName() {
+        this.myName = 'Diana';
+        this.$emit('nameWasReset', this.myName);
       }
+    },
+    created() {
+      eventBus.$on('ageWasEdited', (age) => {
+        this.userAge = age;
+      });
     }
   }
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+  div {
+    background-color: lightcoral;
+  }
 </style>
